ca8b5ac478537de473765db98c0d31bd488fff1e
k-pineda <karenp9310@gmail.com>
2023-04-15 23:54:32 -0400

    added confirm message for each special character

--- Develop/script.js
+++ Develop/script.js
@@ -5,14 +5,21 @@ var generateButton = document.querySelector("#generate");
 
 // Step 2: User is prompted to select
 prompt("How many characters would you like your password to contain?")
+
 // Step 3: User confirms to include special characters 
 confirm("Click Ok to confirm including special characters.")
 var specialCharacters = [" '!','#','$','%','&',''','(','')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\',']','^','_',`{','|','}','~'"]
+
 // Step 4: User confirms to include numeric characters 
+confirm("Click Ok to confirm including numeric characters.")
 var numericCharacters = ["'0','1','2','3','4','5','6','7','8','9'"]
+
 // Step 5: User confirms to include lowercase characters 
+confirm("Click Ok to confirm including lowercase characters.")
 var lowercaseCharacters = ["'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'"]
+
 // Step 6: User confirms to include uppercase characters 
+confirm("Click Ok to confirm including uppercase characters.")
 var uppercaseCharacters=["'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'"]
 
 
