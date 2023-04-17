0b65b1c40eac5fbfd7e2d8f93bee80b436c96b7c
k-pineda <karenp9310@gmail.com>
2023-04-17 17:12:57 -0400

    corrected character values in variables & applied generatedpassword function to each character variable

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,12 @@
 var generateBtn = document.querySelector("#generate");
 
 // User is prompted to select password length
-var specialCharacters = [" '!','#','$','%','&',''','(','')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\',']','^','_',`{','|','}','~'"]
-var userChoice= ""
+var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharacters = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","^"]
+var lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppercaseCharacters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+var userChoice= 5
 
 // function to randomly select special characters
 function getrandomNumber(min, max) {
@@ -14,13 +18,20 @@ function getrandomNumber(min, max) {
 
   return Math.floor(randomNumberinRange);
 }
-function generatePassword(specialCharacters) {
-  var randomArrayPosition = getrandomNumber(0, specialCharacters.length)
+function generatePassword(array) {
+  var randomArrayPosition = getrandomNumber(0, array.length)
 
-  return specialCharacters[randomArrayPosition];
+  return array[randomArrayPosition];
 }
 
-
+var computerChoice = generatePassword(numericCharacters);
+console.log (computerChoice);
+var computerChoice2 = generatePassword (specialCharacters);
+console.log (computerChoice2);
+var computerChoice3 = generatePassword(lowercaseCharacters);
+console.log (computerChoice3);
+var computerChoice4 = generatePassword(uppercaseCharacters);
+console.log (computerChoice4)
 
 // Write password to the #password input
 function writePassword() {
