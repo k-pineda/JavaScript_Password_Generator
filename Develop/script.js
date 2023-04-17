17f18a359f51e0a67f58bcaee775f37d4451de6e
k-pineda <karenp9310@gmail.com>
2023-04-17 13:53:30 -0400

    restructured script

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,37 @@
 // Assignment Code
 // User clicks on generate button
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click",generatePassword);
 
 // User is prompted to select password length
+var specialCharacters = [" '!','#','$','%','&',''','(','')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\',']','^','_',`{','|','}','~'"]
+var userChoice= ""
 
 // function to randomly select special characters
-function generatePassword(specialCharacters) {
-  prompt("How many characters would you like your password to contain?");
-  var includeSpecialCharacters = [" '!','#','$','%','&',''','(','')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\',']','^','_',`{','|','}','~'"];
-  var passwordLength = userChoice;
+function getrandomNumber(min, max) {
+  var randomNumber = Math.random()
+  var randomNumberUptoMax = randomNumber * max
+  var randomNumberinRange = min + randomNumberUptoMax
 
-  for (var i=0; i<= passwordLength; i++)
-  var randomNumber = Math.floor(Math.random() * specialCharacters.length);
-  password += specialCharacters.substring(randomNumber,randomNumber +1);
+  return Math.floor(randomNumberinRange);
+}
+function generatePassword(specialCharacters) {
+  var randomArrayPosition = getrandomNumber(0, specialCharacters.length)
+
+  return specialCharacters[randomArrayPosition];
 }
 
-var computerChoice=generatePassword;
-var userChoice= 1
+
 
 // Write password to the #password input
 function writePassword() {
+  prompt("How many characters would you like your password to contain?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-//conditional statements 
-if (userChoice>=1 && computerChoice>=1) {
-  writePassword ()
-}
 
-else if (userChoice<=0 && computerChoice<=0) {
-  alert("Please choose at least 1 character or more")
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
